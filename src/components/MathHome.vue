187c312fdf6ad73124881416cf12b3210ddc26de
Josh Weinstein <yamaotoko@gmail.com>
2025-01-20 15:51:45 -0500

    implement resetAll on select new math problem; code cleanup

--- src/components/MathHome.vue
+++ src/components/MathHome.vue
@@ -15,7 +15,11 @@ var showProblem = false;
 let random1 = ref(0);
 var random2 = ref(0);
 let gameStarted = ref(false);
-let corrIncorrMsg = '';
+let corrIncorrMsg = ref('');
+let correct = ref(false);
+let error = ref(false);
+let showFeedBack = ref(false);
+//let userInput = ref('');
 
 watch(random1, (newR1, oldR1) => {
   console.log('old random1 = ' + oldR1 + '; new random1 = ' + newR1 );
@@ -28,6 +32,22 @@ const setProblemText = computed(() => {
   return gameStarted.value ? 'Want a new math problem?' : 'Click Here to Start!';
 });
 
+const feedbackMsg = computed(() => {
+  return `Your answer was: ${ userInput }. ${ corrIncorrMsg.value }`;
+});
+
+const correctState = computed(() => {
+  return correct.value;
+});
+
+const errorState = computed(() => {
+  return error.value;
+});
+
+const hasFeedback = computed(() => {
+  return showFeedBack.value;
+});
+
 function generateTwoRandomNums(){
   let r1, r2;
   r1 = Math.floor(Math.random() * randomMax);
@@ -36,17 +56,25 @@ function generateTwoRandomNums(){
   return [r1,r2];
 }
 
-function popAnswerMultiply() {
-  //userInput = document.getElementById('userProvidedAnswer').value;
+function checkAnswer() {
   console.log('userInput = '+ userInput);
   console.log(`Random1 is ${random1.value} and Random2 is ${random2.value}!!`)
   let corrAnswer = random1.value * random2.value;
   let msg = 'The correct answer is : ' + corrAnswer + '.';
   let isCorrect = (Number(userInput) === corrAnswer);
-  corrIncorrMsg = isCorrect ? ' Great job!' : " I'm sorry--pls try again...";
-  alert(' Your answer was: ' + userInput + corrIncorrMsg);
+  showFeedBack.value = true;
+  if(isCorrect) {
+    correct.value = true;
+    error.value = false;
+  } else {
+    correct.value = false;
+    error.value = true;
+  }
+  corrIncorrMsg.value = isCorrect ? ' Great job!' : " I'm sorry--pls try again...";
+  //alert(' Your answer was: ' + userInput + corrIncorrMsg.value);
 }
 function setMathProblem() {
+  resetAll()
   if(!gameStarted.value) {
     gameStarted.value = true;
     console.log('GAME ON!');
@@ -54,14 +82,15 @@ function setMathProblem() {
   const nums = generateTwoRandomNums();
   random1.value = nums[0];
   random2.value = nums[1];
-  // showProblem = true;
-
+  userInput = '';
   console.log(`Random1 is ${nums[0]} and Random2 is ${nums[1]}!!`)
-  // let corrAnswer = nums[0] * nums[1];
-  // let msg = 'The correct answer is : ' + corrAnswer + '.';
-  // let isCorrect = (Number(userInput) === corrAnswer);
-  // let corrIncorrMsg = isCorrect ? ' Great job!' : " I'm sorry--pls try again...";
-  // alert(msg + ' Your answer was: ' + Number(userInput) + corrIncorrMsg);
+}
+
+function resetAll() {
+  corrIncorrMsg.value = '';
+  correct.value = false;
+  error.value = false;
+  showFeedBack.value = false;
 }
 //computed 
 
@@ -74,7 +103,7 @@ function setMathProblem() {
         Header Text--Math Practice
         <div class="row centeredRow">
           <h1>{{ msg }}</h1>
-          <button class="btn btn-primary w-50" @click="setMathProblem()"> {{ setProblemText }} </button>
+          <button class="btn btn-primary w-50" @click="setMathProblem()"> Want a new math problem? </button>
 
         </div>
         <br>
@@ -83,19 +112,17 @@ function setMathProblem() {
               <p class="ms3">What is {{ random1 }} x {{ random2 }}?</p>
           </div>
           <!-- <div class="ms-3">You've chosen to attempt the problem! Showproblem? {{ showProblem }}</div> -->
-          <input v-model.number="userInput" class="form-control w-25 ms-3 centeredInput" @keyup.enter="popAnswerMultiply()" type="number" v-model="userInput">
+          <input v-model="userInput" class="form-control w-25 ms-3 centeredInput" @keyup.enter="checkAnswer()" type="number">
           <br>
           <div class="row centeredRow">
-            <button class="btn btn-secondary w-50 mb-2" @click="popAnswerMultiply()"> Check Answer! </button>
-            <!-- <div class="col-12"> -->
-            <!-- </div> -->
+            <button class="btn btn-secondary w-50 mb-2" @click="checkAnswer()"> Check Answer! </button>
           </div>
         </div>
-
-        <!-- <div class="input-group mb-3">
-          <input id="userProvidedAnswer" @keyup.enter="popAnswerMultiply()" type="number" class="form-control" placeholder="Your answer here . . . " aria-label="your answer" aria-describedby="basic-addon2">
-          <span @click="popAnswerMultiply()"  class="input-group-text" id="basic-addon2">Check Answer!</span>
-        </div> -->
+        <div v-if="hasFeedback" class="card w-100 ms-3 me-3 mt-3 feedbackCard" :class="[ {correct : correctState}, {error : errorState} ]">
+          <div class="row centeredRow" >
+              <p class="ms3">{{ feedbackMsg }}</p>
+          </div>
+        </div>
     </div>
 </template>
 <style lang="css">
@@ -111,5 +138,13 @@ function setMathProblem() {
 }
 p.ms3, h1 {
     text-align: center;
+}
+.feedbackCard {
+  &.correct {
+    background-color: aquamarine;
+  }
+  &.error {
+    background-color: coral;
+  }
 }
 </style>
